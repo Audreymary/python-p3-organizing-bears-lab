1a2a905c20fc813edb36aef57e3fa1232fa220d3
Audreymary <audreymoraa006@gmail.com>
2024-12-03 15:05:30 +0300

    add initial commit

--- .history/lib/sql_queries_20241203144422.py
+++ .history/lib/sql_queries_20241203144422.py
@@ -1,5 +1,6 @@
 select_all_female_bears_return_name_and_age = """
-    SELECT
+    Write your SQL query here
+        SELECT
         bears.name,
         bears.age
     FROM bears
@@ -7,35 +8,21 @@ select_all_female_bears_return_name_and_age = """
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    SELECT
+    Write your SQL query here
+       SELECT
         bears.name
     FROM bears
     ORDER BY bears.name ASC;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    SELECT
-      bears.name,
-      bears.age
-    FROM bears
-    WHERE alive=1
-    ORDER BY bears.age ASC;
+    Write your SQL query here
+      
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-    SELECT
-      bears.name,
-      bears.age
-    FROM bears
-    ORDER BY bears.age DESC
-    LIMIT 1;
+    Write your SQL query here
 """
-
 select_youngest_bear_and_returns_name_and_age = """
-    SELECT
-      bears.name,
-      bears.age
-    FROM bears
-    ORDER BY bears.age ASC
-    LIMIT 1;
+    Write your SQL query here
 """
